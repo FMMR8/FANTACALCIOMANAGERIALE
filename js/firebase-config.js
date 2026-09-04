7bac65b498de5cbd6eddabee8e7139b467edac96
RAITI <mattia.raiteri@libero.it>
2026-09-04 02:13:00 +0200

    Delete js directory

--- js/firebase-config.js
+++
@@ -1,8 +0,0 @@
-export const firebaseConfig = {
-  apiKey: "AIzaSyCjVdjsjqPwWSIDiuuG6k8xzX0JxiB8n0k",
-  authDomain: "fantacalciomanageriale-8.firebaseapp.com",
-  projectId: "fantacalciomanageriale-8",
-  storageBucket: "fantacalciomanageriale-8.firebasestorage.app",
-  messagingSenderId: "73329408803",
-  appId: "1:73329408803:web:b070d015a95a952f6caa96",
-};
